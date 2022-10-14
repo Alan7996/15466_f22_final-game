cbae2b9949baca28db6497b4de19d26b90e626ee
Alan <my_ant1@naver.com>
2022-10-14 12:59:23 -0400

    Initial setup

---
+++ PlayMode.hpp
@@ -0,0 +1,39 @@
+#include "Mode.hpp"
+
+#include "Scene.hpp"
+#include "WalkMesh.hpp"
+
+#include <glm/glm.hpp>
+
+#include <vector>
+#include <deque>
+
+struct PlayMode : Mode {
+	PlayMode();
+	virtual ~PlayMode();
+
+	//functions called by main loop:
+	virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
+	virtual void update(float elapsed) override;
+	virtual void draw(glm::uvec2 const &drawable_size) override;
+
+	//----- game state -----
+
+	//input tracking:
+	struct Button {
+		uint8_t downs = 0;
+		uint8_t pressed = 0;
+	} left, right, down, up;
+
+	//local copy of the game scene (so code can change it during gameplay):
+	Scene scene;
+
+	//player info:
+	struct Player {
+		WalkPoint at;
+		//transform is at player's feet and will be yawed by mouse left/right motion:
+		Scene::Transform *transform = nullptr;
+		//camera is at player's head and will be pitched by mouse up/down motion:
+		Scene::Camera *camera = nullptr;
+	} player;
+};
